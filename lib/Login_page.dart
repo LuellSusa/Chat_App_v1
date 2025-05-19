58d1635c87fcb9ac51d9d07a359ea0e1003ad487
LuellSusa <isaiahbriones021@gmail.com>
2025-05-19 18:01:27 +0800

    Placements and Alignments

--- lib/Login_page.dart
+++ lib/Login_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(LoginPage());
+  runApp(MaterialApp(home: LoginPage()));
 }
 
 class LoginPage extends StatelessWidget {
@@ -18,46 +18,38 @@ class LoginPage extends StatelessWidget {
         },
         child: const Icon(Icons.add),
       ),
-      body: Column(
-        children: [
-          Text(
-            'Lets sign you in!',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.brown,
-              letterSpacing: 0.5,
-            ),
-          ),
-          Text(
-            'Welcome back! \nYou\'ve been missed',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.blue,
-            ),
-          ),
-          Image.network(
-            'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
-            height: 100,
-            width: 100,
-          ),
-          Container(
-            height: 150,
-            width: 150,
-            child: FlutterLogo(),
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                    'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Lets sign you in!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.brown,
+                letterSpacing: 0.5,
               ),
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(10),
             ),
-          )
-        ],
+            SizedBox(height: 10),
+            Text(
+              'Welcome back!\nYou\'ve been missed',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 20),
+            Image.network(
+              'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+              height: 100,
+              width: 100,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -9,7 +9,7 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text('Haluuu, Briones'),
+        title: Text('Goodmorning, StreetPhrmacst'),
         actions: [
           IconButton(
             onPressed: () {
@@ -21,96 +21,109 @@ class ChatPage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Kumusta ang buhay2?', style: TextStyle(fontSize: 20)),
-                Image.network(
-                  'https://em-content.zobj.net/thumbs/240/apple/354/dizzy-face_1f635.png',
-                  height: 80,
-                  width: 80,
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Kumusta ang buhay2?', style: TextStyle(fontSize: 20)),
+                  Image.network(
+                    'https://em-content.zobj.net/thumbs/240/apple/354/dizzy-face_1f635.png',
+                    height: 80,
+                    width: 80,
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomRight: Radius.circular(12),
                 ),
-              ],
-            ),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-                bottomRight: Radius.circular(12),
               ),
             ),
           ),
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Ako pa ba? :(', style: TextStyle(fontSize: 20)),
-                Image.network(
-                  'https://cdn-icons-png.flaticon.com/512/742/742774.png',
-                  height: 80,
-                  width: 80,
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Ako pa ba? :(', style: TextStyle(fontSize: 20)),
+                  Image.network(
+                    'https://cdn-icons-png.flaticon.com/512/742/742774.png',
+                    height: 80,
+                    width: 80,
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: Radius.circular(12),
                 ),
-              ],
-            ),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-                bottomRight: Radius.circular(12),
               ),
             ),
           ),
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Kapoy naku T_T', style: TextStyle(fontSize: 20)),
-                Image.network(
-                  'https://em-content.zobj.net/thumbs/240/apple/354/dizzy-face_1f635.png',
-                  height: 80,
-                  width: 80,
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Kapoy naku T_T', style: TextStyle(fontSize: 20)),
+                  Image.network(
+                    'https://em-content.zobj.net/thumbs/240/apple/354/dizzy-face_1f635.png',
+                    height: 80,
+                    width: 80,
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomRight: Radius.circular(12),
                 ),
-              ],
-            ),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-                bottomRight: Radius.circular(12),
               ),
             ),
           ),
-          Container(
-            height: 80,
-            child: Row(
-              children: [
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    )),
-                IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.send,
-                      color: Colors.white,
-                    ))
-              ],
-            ),
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              height: 80,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      )),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.send,
+                        color: Colors.white,
+                      ))
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.vertical(top: Radius.circular(10)),
+              ),
             ),
           ),
         ],
