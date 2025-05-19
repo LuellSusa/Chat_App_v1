80873a0472dc025f2e52117c9094a5f848280e96
LuellSusa <isaiahbriones021@gmail.com>
2025-05-20 02:23:44 +0800

    Building dynamic lists

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -24,31 +24,42 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: ListView(
-              children: [
-                ChatBubble(
-                  message: 'Hello, how are you?',
-                  alignment: Alignment.centerLeft,
-                ),
-                ChatBubble(
-                  message: 'Life is complicated',
-                  alignment: Alignment.centerRight,
-                ),
-                ChatBubble(
-                  message: 'i hope you are doing well...',
-                  alignment: Alignment.centerLeft,
-                ),
-                ChatBubble(
-                  message: 'You can do it',
-                  alignment: Alignment.centerLeft,
-                ),
-                ChatBubble(
-                  message: 'i hope you will',
-                  alignment: Alignment.centerLeft,
-                ),
-              ],
-            ),
-          ),
+              child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ChatBubble(
+                alignment: index % 2 == 0
+                    ? Alignment.centerLeft
+                    : Alignment.centerRight,
+                message: "Hello",
+              );
+            },
+          )
+              // child: ListView(
+              //   children: [
+              //     ChatBubble(
+              //       message: 'Hello, how are you?',
+              //       alignment: Alignment.centerLeft,
+              //     ),
+              //     ChatBubble(
+              //       message: 'Life is complicated',
+              //       alignment: Alignment.centerRight,
+              //     ),
+              //     ChatBubble(
+              //       message: 'i hope you are doing well...',
+              //       alignment: Alignment.centerLeft,
+              //     ),
+              //     ChatBubble(
+              //       message: 'You can do it',
+              //       alignment: Alignment.centerLeft,
+              //     ),
+              //     ChatBubble(
+              //       message: 'i hope you will',
+              //       alignment: Alignment.centerLeft,
+              //     ),
+              //   ],
+              // ),
+              ),
           ChatInput(),
         ],
       ),
