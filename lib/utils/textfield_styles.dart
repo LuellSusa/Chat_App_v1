126228d5b4955cc018d9b2b6bcb9d68f94eaa245
LuellSusa <isaiahbriones021@gmail.com>
2025-05-27 21:02:34 +0800

    Exploring pub.dev

--- lib/utils/textfield_styles.dart
+++ lib/utils/textfield_styles.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:ui';
 
 class ThemeTextFieldStyle {
-  static TextStyle loginTextFieldStyle = TextStyle(color: Colors.blueGrey);
+  static TextStyle loginTextFieldStyle = GoogleFonts.poppins(
+    textStyle: TextStyle(color: Colors.blueGrey),
+  );
 }
