b2b2a96bc54d16b97d303ffe14d9b807a6c9776e
LuellSusa <isaiahbriones021@gmail.com>
2025-05-27 23:20:25 +0800

    Generics in Dart

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,50 +1,64 @@
+import 'package:chat_app/widgets/chat_bubble.dart';
+import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
+
 import 'models/chat_message_entity.dart';
-import 'widgets/chat_bubble.dart';
-import 'widgets/chat_input.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  ChatPage({Key? key}) : super(key: key);
+
+  List<ChatMessageEntity> _messages = [
+    ChatMessageEntity(
+        author: Author(userName: 'StreetPhilosopher'),
+        createdAt: 2131231242,
+        id: '1',
+        text: 'First text'),
+    ChatMessageEntity(
+        author: Author(userName: 'StreetPhilosopher'),
+        createdAt: 2131231442,
+        id: '1',
+        text: 'Second text',
+        imageUrl: 'https://3009709.youcanlearnit.net/Alien_LIL_131338.png'),
+    ChatMessageEntity(
+      author: Author(userName: 'jane'),
+      createdAt: 2131234242,
+      id: '1',
+      text: 'Third text',
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
     final username = ModalRoute.of(context)!.settings.arguments as String;
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.blue,
         elevation: 0,
         title: Text('Hi $username!'),
         actions: [
           IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/');
-              print('Logged out');
-            },
-          )
+              onPressed: () {
+                //TODO: Navigate back to LoginPage on logout
+                Navigator.pushReplacementNamed(context, '/');
+                print('Icon press');
+              },
+              icon: Icon(Icons.logout))
         ],
       ),
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return ChatBubble(
-                  alignment: index % 2 == 0
-                      ? Alignment.centerLeft
-                      : Alignment.centerRight,
-                  entity: ChatMessageEntity(
-                    id: 'msg_$index',
-                    text: 'Message #$index from $username',
-                    createdAt: DateTime.now().millisecondsSinceEpoch,
-                    author: Author(userName: username),
-                  ),
-                );
-              },
-            ),
-          ),
+              child: ListView.builder(
+                  itemCount: _messages.length,
+                  itemBuilder: (context, index) {
+                    return ChatBubble(
+                        alignment: _messages[index].author.userName ==
+                                'StreetPhilosopher'
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        entity: _messages[index]);
+                  })),
+          ChatInput(),
         ],
       ),
     );
---
+++ lib/demo/counter_stateful_demo.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CounterStateful extends StatefulWidget {
+  //Widget variable should be final
+  final Color buttonColor;
+
+  CounterStateful({Key? key, required this.buttonColor}) : super(key: key);
+
+  @override
+  State<CounterStateful> createState() => _CounterStatefulState();
+}
+
+class _CounterStatefulState extends State<CounterStateful> {
+  int counter = 0;
+
+  void increment() {
+    if (mounted) {
+      setState(() {
+        counter++;
+      });
+    }
+
+    print(counter);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: widget.buttonColor,
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+      body: Center(
+        child: Text(
+          '$counter',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
